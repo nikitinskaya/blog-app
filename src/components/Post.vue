1ec75198e2033e0e4bda66c058ce729e167cd677
Vera Nikitinskaya <nikitinskaya.v@gmail.com>
2019-01-17 01:12:17 +0300

    Add post card component

---
+++ src/components/Post.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+      <div class="box">
+        <button class="delete is-pulled-right"></button>
+        <div class="media">
+          <p>
+            <strong>John Smith</strong>
+            <br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
+          </p>
+        </div>
+      </div>
+  </div>
+</template>
--- src/views/Posts.vue
+++ src/views/Posts.vue
@@ -1,3 +1,14 @@
 <template>
+  <Post/>
 </template>
 
+<script>
+// @ is an alias to /src
+import Post from "@/components/Post.vue";
+
+export default {
+  components: {
+    Post
+  }
+};
+</script>
