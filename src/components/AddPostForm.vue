7898d504177ac61391c26d5d00564dfec11fcb31
Vera Nikitinskaya <nikitinskaya.v@gmail.com>
2019-01-17 02:12:12 +0300

    Move form to separate component

---
+++ src/components/AddPostForm.vue
@@ -0,0 +1,33 @@
+<template>
+  <form class="box">
+    <div class="field">
+      <label class="label">Title</label>
+      <div class="control">
+        <input class="input" type="text" placeholder="e.g My Awesome Adventure">
+      </div>
+    </div>
+
+    <div class="field">
+      <label class="label">Author</label>
+      <div class="control">
+        <input class="input" type="text" placeholder="e.g Alex Smith">
+      </div>
+    </div>
+
+    <div class="field">
+      <label class="label">Post text</label>
+      <div class="control">
+        <textarea class="textarea" placeholder="Write your post here" rows="8"></textarea>
+      </div>
+    </div>
+
+    <div class="field is-grouped">
+      <div class="control">
+        <button class="button is-link">Submit</button>
+      </div>
+      <div class="control">
+        <button class="button is-text">Cancel</button>
+      </div>
+    </div>
+  </form>
+</template>
--- src/views/AddPost.vue
+++ src/views/AddPost.vue
@@ -1,35 +1,16 @@
 <template>
   <div class="container">
-    <form class="box">
-      <div class="field">
-        <label class="label">Title</label>
-        <div class="control">
-          <input class="input" type="text" placeholder="e.g My Awesome Adventure">
-        </div>
-      </div>
-
-      <div class="field">
-        <label class="label">Author</label>
-        <div class="control">
-          <input class="input" type="text" placeholder="e.g Alex Smith">
-        </div>
-      </div>
-
-      <div class="field">
-        <label class="label">Post text</label>
-        <div class="control">
-          <textarea class="textarea" placeholder="Write your post here" rows="8"></textarea>
-        </div>
-      </div>
-
-      <div class="field is-grouped">
-        <div class="control">
-          <button class="button is-link">Submit</button>
-        </div>
-        <div class="control">
-          <button class="button is-text">Cancel</button>
-        </div>
-      </div>
-    </form>
+    <AddPostForm/>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import AddPostForm from "@/components/AddPostForm.vue";
+
+export default {
+  components: {
+    AddPostForm
+  }
+};
+</script>
