a2d9cda7b43ba1422e0f6b91dc255f04c922b903
Vera Nikitinskaya <nikitinskaya.v@gmail.com>
2019-01-17 02:09:42 +0300

    Add post page

--- src/router.js
+++ src/router.js
@@ -21,6 +21,14 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/AddPost.vue')
+    },
+    {
+      path: '/post/1',
+      name: 'post',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/FullPost.vue')
     }
   ]
 })
---
+++ src/views/FullPost.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <div class="box">
+      <button class="delete is-pulled-right"></button>
+      <div class="media">
+        <article class="post-text">
+          <h2 class="title is-4 is-inline-block">A post</h2>
+          <h3 class="subtitle is-6">
+            <strong>John Smith</strong>
+          </h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.</p>
+          <button class="delete-post button is-outlined is-link">Delete</button>
+        </article>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .delete-post {
+      margin-top: 2em;
+  }
+</style>
