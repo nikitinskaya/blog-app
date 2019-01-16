ccc27b38abd94ed953323ae3f57cfdd3167d89f2
Vera Nikitinskaya <nikitinskaya.v@gmail.com>
2019-01-17 01:41:07 +0300

    Fix post card design

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,13 +1,36 @@
 <template>
   <div class="container">
-      <div class="box">
-        <button class="delete is-pulled-right"></button>
-        <div class="media">
-          <p>
+    <div class="box">
+      <div class="media">
+        <article class="post-text">
+          <h2 class="title is-4 is-inline-block">
+            <router-link to="/post/1">A post</router-link>
+          </h2>
+          <h3 class="subtitle is-6">
             <strong>John Smith</strong>
-            <br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
-          </p>
-        </div>
+          </h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis. </p>
+        </article>
       </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+  .post-text {
+    position: relative;
+    overflow: hidden;
+    white-space: nowrap;
+  }
+
+  .post-text::after {
+    content: "";
+    background-image: linear-gradient(to right, rgba(255,0,0,0), rgba(256,256,256,1));
+    height: 1.5em;
+    width: 3em;
+    position: absolute;
+    right: 0;
+    bottom: 0;
+
+  }
+</style>
