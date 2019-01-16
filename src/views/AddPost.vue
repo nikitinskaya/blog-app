feb4a16cef0949c1279a3ec9855316ed0a9f7d0d
Vera Nikitinskaya <nikitinskaya.v@gmail.com>
2019-01-17 01:23:20 +0300

    Add new post form

--- src/views/AddPost.vue
+++ src/views/AddPost.vue
@@ -1,5 +1,35 @@
 <template>
-  <div class="about">
-    <h1>Add new post here</h1>
+  <div class="container">
+    <form class="box">
+      <div class="field">
+        <label class="label">Title</label>
+        <div class="control">
+          <input class="input" type="text" placeholder="e.g My Awesome Adventure">
+        </div>
+      </div>
+
+      <div class="field">
+        <label class="label">Author</label>
+        <div class="control">
+          <input class="input" type="text" placeholder="e.g Alex Smith">
+        </div>
+      </div>
+
+      <div class="field">
+        <label class="label">Post text</label>
+        <div class="control">
+          <textarea class="textarea" placeholder="Write your post here" rows="8"></textarea>
+        </div>
+      </div>
+
+      <div class="field is-grouped">
+        <div class="control">
+          <button class="button is-link">Submit</button>
+        </div>
+        <div class="control">
+          <button class="button is-text">Cancel</button>
+        </div>
+      </div>
+    </form>
   </div>
 </template>
