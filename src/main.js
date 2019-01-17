9717dcd84644691d2f838900bb927970a8b95277
Vera Nikitinskaya <nikitinskaya.v@gmail.com>
2019-01-17 22:55:25 +0300

    Add buefy

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import 'bulma/css/bulma.css'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
 
 Vue.config.productionTip = false
+Vue.use(Buefy);
 
 new Vue({
   router,
