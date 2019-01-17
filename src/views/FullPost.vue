118fea89d720045e9e3d9672736c93a559ee2ee9
Vera Nikitinskaya <nikitinskaya.v@gmail.com>
2019-01-17 23:48:34 +0300

    Render post data in full post

--- src/views/FullPost.vue
+++ src/views/FullPost.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="container">
-    <div class="box">
+    <div class="box post-box">
       <button class="delete is-pulled-right"></button>
       <div class="media">
         <article class="post-text">
-          <h2 class="title is-4 is-inline-block">A post</h2>
+          <h2 class="title is-4 is-inline-block">{{ post.title }}</h2>
+          <b-taglist class="post-tags is-inline-block">
+            <b-tag class="post-tag" v-for="tag in post.tags" :key="tag">{{ tag }}</b-tag>
+          </b-taglist>
           <h3 class="subtitle is-6">
-            <strong>John Smith</strong>
+            <strong>{{ post.author }}</strong>
           </h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.</p>
+          <p>{{ post.text }}</p>
           <button class="delete-post button is-outlined is-link">Delete</button>
         </article>
       </div>
@@ -17,28 +20,53 @@
 </template>
 
 <script>
-import PostsService from '@/services/PostsService'
+import PostsService from "@/services/PostsService";
 export default {
-  name: 'post',
-  data () {
+  name: "post",
+  data() {
     return {
       post: {}
-    }
+    };
   },
-  mounted () {
-    this.getPost()
+  mounted() {
+    this.getPost();
   },
   methods: {
-    async getPosts () {
-      const response = await PostsService.fetchPosts()
-      this.posts = response.data.posts
+    async getPosts() {
+      const response = await PostsService.fetchPosts();
+      this.posts = response.data.posts;
+    },
+    async getPost() {
+      const response = await PostsService.getPost({
+        id: this.$route.params.id
+      });
+      this.post = response.data;
+      console.log(this.post);
     }
   }
-}
+};
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .delete-post {
   margin-top: 2em;
+}
+.post-box {
+  @media (max-width: 1087px) {
+    margin-left: 1.5em;
+    margin-right: 1.5em;
+  }
+}
+.post-tags {
+  margin-bottom: 0;
+  margin-left: 2em;
+}
+
+.post-tag {
+  margin-bottom: 0;
+}
+
+.title {
+  margin-bottom: 0;
 }
 </style>
